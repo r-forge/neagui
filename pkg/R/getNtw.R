188fcf7fb8d38353e6a74ae6a8e14925b3fed9ef
setiapramana (Setia Pramana) <setia.pramana@ki.se>
2013-06-04 06:38:13 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/neagui@47 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/getNtw.R
+++ pkg/R/getNtw.R
@@ -14,29 +14,32 @@ tkmessageBox(message = "No file was selected!")
 else 
 {
 splitted <- strsplit(fileName,".",fixed=TRUE)
-if(unlist(splitted)[2] == "txt")
+
+splitted2 <- unlist(splitted)
+filetype  <<- splitted2 [length(splitted2 )]
+
+
+if(filetype   == "txt")
 {
  tclvalue(Ntwk) <<- fileName
  network <- scan(file=fileName, sep=",", what=character())
  assign("NETWORK",network,envir=.GlobalEnv)
 }
 
-if(unlist(splitted)[2] == "csv")
+if(filetype   == "csv")
 {
 tclvalue(Ntwk) <<- fileName
 network <- as.character(unlist(read.csv(fileName, header =F)))
 assign("NETWORK",network,envir=.GlobalEnv)
 }
 ####
-if (unlist(splitted)[2] == "RData" | unlist(splitted)[2] == "Rdata")
+if (filetype   == "RData" | filetype   == "Rdata")
 {
-#.tempEnv <- new.env()
-#dataSet <- load(fileName, envir = .tempEnv)
+
 LoadRdata(fileName,"NETWORK")
   tclvalue(Ntwk) <<- paste(inptObjt, "->", fileName)
 	checkObject ("inptObjt")
 
-#rm(.tempEnv)
 }
 #######
 if (filetype %in% c( "RData", "Rdata","txt", "csv") ==F)
